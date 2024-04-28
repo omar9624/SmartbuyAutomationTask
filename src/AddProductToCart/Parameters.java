db7280825c2ad464f808a9c3a872a06d9b8dc15f
omar9624 <omaralsayyed24@gmail.com>
2024-04-29 00:37:21 +0300

    Initial

---
+++ src/AddProductToCart/Parameters.java
@@ -0,0 +1,39 @@
+package AddProductToCart;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Random;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.asserts.Assertion;
+
+public class Parameters {
+	
+	String url = "https://smartbuy-me.com/smartbuystore/en/";
+	
+	WebDriver driver = new ChromeDriver();
+	
+	Assertion myAssert = new Assertion();
+	
+	Random random = new Random();
+	
+	//Screenshot Function
+	public void TakeScreenshot() throws IOException {
+			
+		TakesScreenshot ts = ((TakesScreenshot) driver);
+		
+		LocalDateTime date =  LocalDateTime.now();
+		String filename = date.toString().replace(":", "-");
+			
+		File srcFile = ts.getScreenshotAs(OutputType.FILE);
+		File destFile = new File("./src/images/"+filename+".jpg");
+			
+		srcFile.renameTo(destFile);
+			
+	}
+	
+}
