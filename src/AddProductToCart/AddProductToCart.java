213ff6e7bcb077263484324e6b50e02447002436
omar9624 <omaralsayyed24@gmail.com>
2024-04-29 00:40:23 +0300

    Update

--- src/AddProductToCart/AddProductToCart.java
+++ src/AddProductToCart/AddProductToCart.java
@@ -16,65 +16,64 @@ public class AddProductToCart extends Parameters {
 		driver.get(url);
 		driver.manage().window().maximize();
 	}
-	
+
 	@Test(invocationCount = 1)
 	public void addProduct() throws InterruptedException, IOException {
 		WebElement searchInput = driver.findElement(By.id("js-site-search-input"));
 		searchInput.sendKeys("iphone");
 		searchInput.sendKeys(Keys.ENTER);
-		
-		
-		WebElement PriceFilterList = driver.findElement(By.cssSelector("div[class='facet js-facet head_price'] div[class='facet__name js-facet-name']"));
+
+		WebElement PriceFilterList = driver.findElement(
+				By.cssSelector("div[class='facet js-facet head_price'] div[class='facet__name js-facet-name']"));
 		PriceFilterList.click();
-		
-		WebElement priceListContainer = driver.findElement(By.cssSelector("body > main:nth-child(10) > div:nth-child(12) > div:nth-child(1) > div:nth-child(6) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1)"));
+
+		WebElement priceListContainer = driver.findElement(By.cssSelector(
+				"body > main:nth-child(10) > div:nth-child(12) > div:nth-child(1) > div:nth-child(6) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1)"));
 		List<WebElement> priceList = priceListContainer.findElements(By.tagName("li"));
-		
-		priceList.get(priceList.size()-1).click();
-		
+
+		priceList.get(priceList.size() - 1).click();
+
 		WebElement productContainer = driver.findElement(By.cssSelector(".product__listing.product__grid"));
 		List<WebElement> products = productContainer.findElements(By.xpath("//div[@class='product-item item_grid']"));
-		
-		
+
 		Thread.sleep(1000);
-		
-		for(int i=0 ; i < products.size() ; i++) {
-			
+
+		for (int i = 0; i < products.size(); i++) {
+
 			int randomNumber = random.nextInt(products.size());
 			WebElement productChoosed = products.get(randomNumber);
-			
-		
-		    String priceText = productChoosed.findElement(By.className("price")).getText();
+
+			String priceText = productChoosed.findElement(By.className("price")).getText();
 			System.out.println(randomNumber);
-			
-			if(priceText == "") {
+
+			if (priceText == "") {
 				System.out.println("not Found ");
 				continue;
-			}else {
+			} else {
 				String cleanedAmountString = priceText.replace(",", "").replace(" JOD", ""); // Removes comma and " JOD"
 				int price = Integer.parseInt(cleanedAmountString);
-				//Check If Price Less Than 1100 JOD
-				if(price < 1100) {
+				// Check If Price Less Than 1100 JOD
+				if (price < 1100) {
 					productChoosed.click();
 					System.out.println("less than 1100");
 					break;
-				}else {
+				} else {
 					continue;
 				}
-			}		
-			
+			}
+
 		}
-		
-		//Assertion
+
+		// Assertion
 		WebElement addToCartButton = driver.findElement(By.id("addToCartButton"));
 		System.out.println(addToCartButton.getText());
 		Boolean actualResult = addToCartButton.getText().contains("ADD TO CART");
-		myAssert.assertEquals(actualResult, true , "The Product is Sold Out");
-		
+		myAssert.assertEquals(actualResult, true, "The Product is Sold Out");
+
 		addToCartButton.click();
-		
+
 		Thread.sleep(2000);
 		TakeScreenshot();
-		
+
 	}
 }
--- src/AddProductToCart/Parameters.java
+++ src/AddProductToCart/Parameters.java
@@ -12,28 +12,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.asserts.Assertion;
 
 public class Parameters {
-	
+
 	String url = "https://smartbuy-me.com/smartbuystore/en/";
-	
+
 	WebDriver driver = new ChromeDriver();
-	
+
 	Assertion myAssert = new Assertion();
-	
+
 	Random random = new Random();
-	
-	//Screenshot Function
+
+	// Screenshot Function
 	public void TakeScreenshot() throws IOException {
-			
+
 		TakesScreenshot ts = ((TakesScreenshot) driver);
-		
-		LocalDateTime date =  LocalDateTime.now();
+
+		LocalDateTime date = LocalDateTime.now();
 		String filename = date.toString().replace(":", "-");
-			
+
 		File srcFile = ts.getScreenshotAs(OutputType.FILE);
-		File destFile = new File("./src/images/"+filename+".jpg");
-			
+		File destFile = new File("./src/images/" + filename + ".jpg");
+
 		srcFile.renameTo(destFile);
-			
+
 	}
-	
+
 }
